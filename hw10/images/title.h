57ea8536aad7bf45f2232727a60e8868940368fa
thergautam12 <44377150+thergautam12@users.noreply.github.com>
2018-11-17 12:29:32 -0500

    Add files via upload

---
+++ hw10/images/title.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 --resize=240x160 title title.png 
+ * Time-stamp: Tuesday 11/13/2018, 20:41:49
+ * 
+ * Image Information
+ * -----------------
+ * title.png 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef TITLE_H
+#define TITLE_H
+
+extern const unsigned short title[38400];
+#define TITLE_SIZE 76800
+#define TITLE_LENGTH 38400
+#define TITLE_WIDTH 240
+#define TITLE_HEIGHT 160
+
+#endif
+
